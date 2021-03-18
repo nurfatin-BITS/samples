87b40bf5c174cc5f0601c4b5a40996f6ec6fda13
Nur Fatin Bt Yazid <b031910017@student.utem.edu.my>
2021-03-18 15:31:00 +0800

    Initial Commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,16 @@
+package samples.demo;
+
+public class Location {
+
+	public String location;
+
+
+public String getlocation() {
+	return location;
+}
+
+public void setlocation(String location) {
+	this.location=location;
+}
+
+}
